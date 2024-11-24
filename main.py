90b432ec60d0861e8bae39ac93b6c6d97f6cdf88
MahinAshraful <150500453+MahinAshraful@users.noreply.github.com>
2024-11-24 01:04:37 -0500

    model

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epoch=3)
+
+model.save('handwritten.model')
+
